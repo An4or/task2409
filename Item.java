404199ac6fee9ffe0fb0c6c7b24d757cd3a06506
An4or <anchor_gomel@tut.by>
2017-06-24 14:06:07 +0300

    Initial commit

---
+++ Item.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Sasha on 24.06.2017.
+ */
+public interface Item {
+    public int getId();
+    public double getPrice();
+    public String getTM();
+}
---
+++ Jeans.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Sasha on 24.06.2017.
+ */
+public interface Jeans extends Item {
+    public int getLength();
+    public int getSize();
+}
